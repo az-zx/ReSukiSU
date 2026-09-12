b54ba43c8ca9a677ef12fb364e5f33edff54cfa5
星夜萌 <157605784+az-zx@users.noreply.github.com>
2026-09-12 14:12:01 +0800

    Add files via upload
    Signed-off-by: 星夜萌 <157605784+az-zx@users.noreply.github.com>

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -33,6 +33,14 @@ struct sdesc {
 
 static apk_sign_key_t apk_sign_keys[] = {
     { EXPECTED_SIZE_XINGMENG, EXPECTED_HASH_XINGMENG },
+#ifdef CONFIG_KSU_MULTI_MANAGER_SUPPORT
+#ifdef EXPECTED_SIZE
+    { EXPECTED_SIZE, EXPECTED_HASH }, // Custom
+#endif
+#ifdef EXPECTED_PR_BUILD_SIZE
+    { EXPECTED_PR_BUILD_SIZE, EXPECTED_PR_BUILD_HASH }, // Custom 2 (For PR build)
+#endif
+#endif
 };
 
 static struct sdesc *init_sdesc(struct crypto_shash *alg)
